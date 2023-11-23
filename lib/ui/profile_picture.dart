c7a85053f537805e3509a53775de57a7d5e5c004
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-11-23 12:52:47 +0700

    Add ProfilePicture widget

---
+++ lib/ui/profile_picture.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePicture extends StatelessWidget {
+  const ProfilePicture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+      ),
+      child: Image.asset('assets'),
+    );
+  }
+}
