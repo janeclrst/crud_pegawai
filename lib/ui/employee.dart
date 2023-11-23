c7b43d7cc0b53383798f0fe2869f4dcfe8c4d1b2
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-11-23 13:15:01 +0700

    Refactor employee UI layout

--- lib/ui/employee.dart
+++ lib/ui/employee.dart
@@ -1,14 +1,43 @@
+import 'package:crud_pegawai/ui/profile_picture.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class Employee extends StatelessWidget {
   const Employee({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-        ],
+    return Center(
+      child: IntrinsicHeight(
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const ProfilePicture(),
+              const Gap(16),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Nama'.toUpperCase(),
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                  Text(
+                    'Jabatan',
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  Text(
+                    'no.tlp',
+                    style: TextStyle(color: Colors.black54),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
